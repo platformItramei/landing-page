bab2f473f78ba8f39df0d887ef5b16398e0de126
Ashish <Ashish.Patil@itramei.com>
2025-09-02 15:58:12 +0100

    added tag manage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ function GTMNoscript() {
   return (
     <noscript>
       <iframe
-        src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+        src={`https://www.googletagmanager.com/ns.html?id=GTM-NQ8CBNMN`}
         height="0"
         width="0"
         style={{ display: "none", visibility: "hidden" }}
@@ -51,7 +51,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
+              })(window,document,'script','dataLayer','GTM-NQ8CBNMN');
             `,
           }}
         />
@@ -74,7 +74,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               window.dataLayer = window.dataLayer || [];
               window.gtag = function gtag(){window.dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GTM_ID}');
+              gtag('config', 'GTM-NQ8CBNMN');
             `,
           }}
         />
